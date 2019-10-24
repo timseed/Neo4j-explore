d9317134f586697ee9b8b4def8f02c6d8d2fe47b
Tim Seed <tim@sy-edm.com>
2019-10-24 17:46:55 +0800

    Multi nodes query based linking. Style Sheet

--- Connect.py
+++ Connect.py
@@ -5,26 +5,54 @@ driver = GraphDatabase.driver(uri, auth=("neo4j", "ducati"))
 
 
 def del_all(tx):
+    """
+    delete the links and the nodes
+    """
     tx.run("match(n) "
-           "delete n ")
+           "detach delete n ")
+
 
 def add_people(tx, name, gender, years=21):
     """
     Create a people object with 3 attributes 
     """
     tx.run("CREATE (n:people {name: $value, gender: $gender, age:$years})",
-      value=name,
-      gender=gender,
-      years=years) 
+           value=name,
+           gender=gender,
+           years=years)
+
+
+def link_people(tx):
+    """
+    Link two nodes with a relationship.
+    """
+    tx.run("MATCH (p:people {gender:'M'}), (m:male {gender:'M'}) "
+           "create (p)-[r:ARE_MEN]->(m) ")
+    tx.run("MATCH (p:people {gender:'F'}), (f:female {gender:'F'}) "
+           "create (p)-[r:ARE_WOMEN]->(f) ")
+
+
+def create_gender(tx):
+    tx.run("Create (n:male   {gender:'M'})")
+    tx.run("Create (n:female {gender:'F'})")
 
 
 with driver.session() as session:
     session.read_transaction(del_all)
-    session.read_transaction(add_people, "Snow White","F",22)
-    session.read_transaction(add_people, "Happy","M",62)
-    session.read_transaction(add_people, "Bashful","M",63)
-    session.read_transaction(add_people, "Doc","M",60)
-    session.read_transaction(add_people, "Grumpy","M")
+
+    session.read_transaction(create_gender)
+    session.read_transaction(add_people, "Snow White", "F", 22)
+    session.read_transaction(add_people, "Happy", "M", 62)
+    session.read_transaction(add_people, "Bashful", "M", 63)
+    session.read_transaction(add_people, "Doc", "M", 60)
+    session.read_transaction(add_people, "Grumpy", "M")
+    session.read_transaction(link_people)
+
+
+#
+#
+#
+
 
 print("""In the Web browser please enter this Cypher statement
 
